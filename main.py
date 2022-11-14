8ed5c6ee06e0e4bcec83e55180eff9c856d0e8bb
dm554 <derikjmcnair@gmail.com>
2022-11-13 21:16:02 -0500

    Simple Card Scrape

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello World')
+import requests
+from bs4 import BeautifulSoup
+
+
+# Scrapes all the card data
+def get_card_info(tag):
+    return tag.name == "table" and tag.has_attr("class") and "main" in tag.get("class")
+
+
+# Get Request
+response = requests.get("https://buddyfight.fandom.com/wiki/Drum_Bunker_Dragon")
+if response.status_code != 200:
+    print("Error fetching page")
+    exit()
+else:
+    content = response.content
+
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+print(soup.findAll(get_card_info))

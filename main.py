181df7630ef215e7818ef7c11327eabf008abe47
dm554 <derikjmcnair@gmail.com>
2022-11-15 10:48:28 -0500

    Gets card and ability info from wiki

--- main.py
+++ main.py
@@ -2,20 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Scrapes all the card data
+# Gets card ability information from wiki page
+def get_card_ability(tag):
+    card_text = tag.findAll(name="tr")
+    card_list = []
+
+    for line in card_text:
+        card_lines = line.get_text(separator=" ", strip=True)
+        card_list.append(card_lines)
+
+    ability_index = card_list.index('Ability / Effect') + 1
+    return card_list[ability_index]
+
+
+# Gets specific card information from wiki page
 def get_card_info(tag):
-    return tag.name == "table" and tag.has_attr("class") and "main" in tag.get("class")
+    card_stats = tag.findAll(name="table", class_="main")
+    stats_list = []
+
+    for stat in card_stats:
+        stats = stat.get_text(separator=",", strip=True)
+        stats_list = stats.split(",")
+
+    stats_list.append(get_card_ability(tag))
+    return stats_list
 
 
-# Get Request
+# Get Request for Webpage
 response = requests.get("https://buddyfight.fandom.com/wiki/Drum_Bunker_Dragon")
+
 if response.status_code != 200:
     print("Error fetching page")
     exit()
 else:
     content = response.content
 
-
 soup = BeautifulSoup(response.content, 'html.parser')
 
-print(soup.findAll(get_card_info))
+print(get_card_info(soup))
+
+
+
+
+
+

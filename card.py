d6a56688f67aafc13380f85e7f9d83757c6a608b
dm554 <derikjmcnair@gmail.com>
2023-06-14 18:50:35 -0400

    Added Attributes to Card class

--- card.py
+++ card.py
@@ -1,28 +1,31 @@
 class Card:
     card_type = ""
 
-    def __init__(self, name, world, effect="No effect"):
+    def __init__(self, name, world, attribute, effect="No effect"):
         self.name = name
         self.effect = effect
         self.world = world
+        self.attribute = attribute
+
 
 
 class Monster(Card):
-    def __init__(self, name, world, size, power, critical, defense, effect):
-        super().__init__(name, world, effect)
+    def __init__(self, name, world, size, power, critical, defense, effect, attribute):
+        super().__init__(name, world, attribute, effect)
         self.size = size
         self.power = power
         self.defense = defense
         self.critical = critical
         self.card_type = "Monster"
 
+
     def __str__(self) -> str:
         return "Name: " + self.name + "\nCard Type: " + self.card_type + "\nWorld: " + self.world + "\nSize: " + self.size + "\nPower: " + self.power + "\nCritical: " + self.critical + "\nDefense: " + self.defense + "\nEffect: " + self.effect
 
 
 class Spell(Card):
-    def __init__(self, name, world, effect):
-        super().__init__(name, world, effect)
+    def __init__(self, name, world, effect, attribute):
+        super().__init__(name, world, attribute, effect)
         self.card_type = "Spell"
 
     def __str__(self) -> str:
@@ -30,8 +33,8 @@ class Spell(Card):
 
 
 class Impact(Card):
-    def __init__(self, name, world, effect):
-        super().__init__(name, world, effect)
+    def __init__(self, name, world, effect, attribute):
+        super().__init__(name, world, attribute, effect)
         self.card_type = "Impact"
 
     def __str__(self) -> str:
@@ -39,8 +42,8 @@ class Impact(Card):
 
 
 class Item(Card):
-    def __init__(self, name, world, power, critical, effect):
-        super().__init__(name, world, effect)
+    def __init__(self, name, world, power, critical, effect, attribute):
+        super().__init__(name, world, attribute, effect)
         self.power = power
         self.critical = critical
         self.card_type = "Item"
--- main.py
+++ main.py
@@ -4,48 +4,6 @@ import json
 from bs4 import BeautifulSoup
 
 
-class CardEncoder(json.JSONEncoder):
-    def default(self, obj):
-        switch = {
-            "Monster": {
-                        'name': obj.name, 
-                        'power': obj.power,
-                        'defense': obj.defense,
-                        'crit' : obj.critical,
-                        'world': obj.world, 
-                        'effect': obj.effect,
-                        },
-            "Spell": {  
-                        'name': obj.name, 
-                        'world': obj.world, 
-                        'effect': obj.effect,
-                        },
-            "Item": {
-                        'name': obj.name, 
-                        'power': obj.power,
-                        'crit' : obj.critical,
-                        'world': obj.world, 
-                        'effect': obj.effect,
-                        },
-            "Impact": { 
-                        'name': obj.name, 
-                        'world': obj.world, 
-                        'effect': obj.effect,
-                        }
-        }
-
-        return switch.get(obj.card_type)
-        # if obj.type == "Monster":
-        #     return{'name': obj.name, 'world': obj.world, 'effect': obj.effect}
-        # if obj.type == "Spell":
-        #     return{'name': obj.name, 'world': obj.world, 'effect': obj.effect}
-        # if obj.type == "Item":
-        #     return{'name': obj.name, 'world': obj.world, 'effect': obj.effect}
-        # if obj.type == "Impact":
-        #     return{'name': obj.name, 'world': obj.world, 'effect': obj.effect}
-
- 
-
 # Get Request for Webpage
 def card_page_init(card_webpage):
     response = requests.get(card_webpage)
@@ -108,25 +66,48 @@ def get_card_type(tag):
 
     return card_type
 
+#Gets a list of card attributes
+def grab_card_attributes(info):
+    startIndex = info.index("Attribute") + 1
+    if "Illust" in info or "Design / Illust" in info:
+        if "Illust" in info:
+            endIndex = info.index("Illust")
+        elif "Design / Illust" in info:
+            endIndex = info.index("Design / Illust")
+    else:
+        endIndex = -1
+    attribute_list = info[startIndex : endIndex]
+    attributes = attribute_list[::2]
+    return attributes
+
+
 
 # Creates a card object based on card type and adds to list
+# Make this function modular
 def card_create(tag, c_list):
     card_type = get_card_type(tag)
     info = get_card_info(tag)
+    attribute = grab_card_attributes(info)
+    
     if card_type == "Monster":
         if info[2] == "Kanji" and info[4] == "Kana":
-            c_list.append(card.Monster(info[1], info[19], info[11], info[13], info[15], info[17], info[-1]).__dict__)
+            c_list.append(card.Monster(info[1], info[19], info[11], info[13], info[15], info[17], info[-1], attribute).__dict__)
         else:
-            c_list.append(card.Monster(info[1], info[17], info[9], info[11], info[13], info[15], info[-1]).__dict__)
+            c_list.append(card.Monster(info[1], info[17], info[9], info[11], info[13], info[15], info[-1], attribute).__dict__)
     elif card_type == "Spell":
         if info[2] == "Kanji" and info[4] == "Kana":
-            c_list.append(card.Spell(info[1], info[11], info[-1]).__dict__)
+            if(info[8] == "Translated"):
+                c_list.append(card.Spell(info[1], info[13], info[-1], attribute).__dict__)
+            else:
+                c_list.append(card.Spell(info[1], info[11], info[-1], attribute).__dict__)
+            
         else:
-            c_list.append(card.Spell(info[1], info[9], info[-1]).__dict__)
+            c_list.append(card.Spell(info[1], info[9], info[-1], attribute).__dict__)
     elif card_type == "Impact":
-        c_list.append(card.Impact(info[1], info[11], info[-1]).__dict__)
+        c_list.append(card.Impact(info[1], info[11], info[-1], attribute).__dict__)
     elif card_type == "Item":
-        c_list.append(card.Item(info[1], info[15], info[11], info[13], info[-1]).__dict__)
+        c_list.append(card.Item(info[1], info[15], info[11], info[13], info[-1], attribute).__dict__)
+
 
 
 soup = card_page_init("https://buddyfight.fandom.com/wiki/Booster_Set_1:_Dragon_Chief")
@@ -134,28 +115,13 @@ card_url_list = card_urls(soup)
 card_url_list = card_url_list[1::3]
 card_list = []
 
-
+#Iterates through Card URLs in Set and Creates Card Objects
 for url in card_url_list:
     soup = card_page_init("https://buddyfight.fandom.com/" + url)
     card_create(soup, card_list)
 
-
-#Opens file to write to
-file = open("CardList.json", "w", encoding='utf-8')
-# # iterates through list of card objects and prints data
-# for card in card_list:
-#     print("\n" + card.__str__())
-#     file.write(card.__str__() + "\n"+ "\n")
-
+#Opens/Creates json File and Dumps Card Objects
 with open("CardList.json", 'w') as f:
-    #json.dump(card_list, f, cls=CardEncoder, indent=4)
     json.dump(card_list, f, indent=4)
 
-#file.close()
-
-
-
-
-
-
-
+print("\n--Set Completed--\n")
